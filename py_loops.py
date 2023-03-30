27042a8c678daf47aec46f1e51f5b1fe7fbc781a
Fedor_xp28@student.itstep.org <Fedor_xp28@student.itstep.org>
2023-03-30 17:51:24 +0300

    Task 2 Done. 1. All numbers in the range.

--- py_loops.py
+++ py_loops.py
@@ -1,2 +1,13 @@
-# Module 2: Branching operators, cycles, exceptions. 
-# Topic: Cycles. Part 1
+# Description: Cycles
+# Version: 1.0
+# Task 2 - Cycles - Part 1 - 1. All numbers in the range.
+
+if __name__ == "__main__":
+    try:
+        start = int(input("Enter the start value: "))
+        end = int(input("Enter the end value: "))
+
+        for i in range(start, end+1):
+                print(i)
+    except ValueError:
+        print("Please enter a valid number")
