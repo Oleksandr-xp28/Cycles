37c753ba34c779370612e71c48e534970d2e7c45
Fedor_xp28@student.itstep.org <Fedor_xp28@student.itstep.org>
2023-03-30 18:18:26 +0300

    Task 2 - Cycles - Part 1 - 2. All numbers in the range in descending order.

--- py_loops.py
+++ py_loops.py
@@ -1,2 +1,14 @@
-# Module 2: Branching operators, cycles, exceptions. 
-# Topic: Cycles. Part 1
+# Description: Cycles
+# Version: 1.0
+# Task 2 - Cycles - Part 1 - 2. All numbers in the range in descending order.
+
+if __name__ == "__main__":
+    try:
+        start = int(input("Enter the starting number: "))
+        end = int(input("Enter the ending number: "))
+
+        for i in range(start, end-1, -1):
+            if i :
+                print(i)
+    except ValueError:
+        print("Please enter a valid number")
