3b2c93040151014844259c9ecd50373b5f3b432a
Fedor_xp28@student.itstep.org <Fedor_xp28@student.itstep.org>
2023-03-30 18:52:03 +0300

    Task 6 done

--- py_loops.py
+++ py_loops.py
@@ -1,2 +1,19 @@
-# Module 2: Branching operators, cycles, exceptions. 
-# Topic: Cycles. Part 1
+# Description: Cycles
+# Version: 1.0
+
+if __name__ == "__main__":
+    try:
+        begin = int(input('Enter a begin: ')) 
+        end = int(input('Enter an end: '))
+
+        for i in range(begin, end+1):
+            if i % 3 == 0 and i % 5 == 0:
+                print("Fizz Buzz")
+            elif i % 3 == 0:
+                print("Fizz")
+            elif i % 5 == 0:
+                print("Buzz")
+            else:
+                print(i)
+    except ValueError:
+        print("Please enter a valid number")
