1d7b03712b88c70692b4a1184eb5cbd89b88ca95
Fedor_xp28@student.itstep.org <Fedor_xp28@student.itstep.org>
2023-03-30 17:31:38 +0300

    ex_1 task done

--- py_loops.py
+++ py_loops.py
@@ -1,2 +1,6 @@
-# Module 2: Branching operators, cycles, exceptions. 
-# Topic: Cycles. Part 1
+start = int(input("Enter the start value: "))
+end = int(input("Enter the end value: "))
+
+for i in range(start, end):
+    if i % 7 == 0:
+        print(i)
