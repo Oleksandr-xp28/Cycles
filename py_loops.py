35c7435d28854ea36e45d247980c4d3be104b17c
Fedor_xp28@student.itstep.org <Fedor_xp28@student.itstep.org>
2023-03-30 18:30:56 +0300

    Task Done.. All numbers in multiples of 7.

--- py_loops.py
+++ py_loops.py
@@ -1,2 +1,15 @@
-# Module 2: Branching operators, cycles, exceptions. 
-# Topic: Cycles. Part 1
+# Description: Cycles
+# Version: 1.0
+# Task 2 - Cycles - Part 1 - 3. All numbers in multiples of 7.
+
+if __name__ == "__main__":
+    try:
+        start = int(input("Enter the starting number: "))
+        end = int(input("Enter the ending number: "))
+
+        for i in range(start, end+1):
+            if i % 5 == 0:
+                print(i)
+        print("The end")
+    except ValueError:
+        print("Please enter a valid number")
