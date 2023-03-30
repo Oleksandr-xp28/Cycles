563633235f88d3694d4748d61193abe363c3106b
Fedor_xp28@student.itstep.org <Fedor_xp28@student.itstep.org>
2023-03-30 17:37:23 +0300

    Update

--- py_loops.py
+++ py_loops.py
@@ -1,3 +1,15 @@
+if __name__ == "__main__":
+    try:
+        start = int(input("Enter the start value: "))
+        end = int(input("Enter the end value: "))
+
+        for i in range(start, end):
+            if i % 7 == 0:
+                print(i)
+    except :
+        print("Error")
+        
+
 start = int(input("Enter the start value: "))
 end = int(input("Enter the end value: "))
 
